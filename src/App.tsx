ca239ee198dba6c216348f8f68ad942eeb834af3
a-sha0234 <asharma_31@hotmail.com>
2022-07-07 13:24:31 +0100

    create base files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">dsf</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          {" "}
+          <Navbar />
+        </div>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
