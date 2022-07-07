d35d870d4450c63b4ee51170ae122d45ca57e97c
a-sha0234 <asharma_31@hotmail.com>
2022-07-07 21:01:14 +0100

    update data with projects

--- src/data.tsx
+++ src/data.tsx
@@ -1,7 +1,60 @@
+import restaurentImg from "./screenshots/restaurent.png";
+import ticTacToe from "./screenshots/ticTacToe.png";
+import blog from "./screenshots/blog.png";
+import receipt from "./screenshots/receipt.png";
+
 interface Project {
   id: number;
+  name: string;
+  githubFrontRepo: string;
+  githubBackRepo: string;
+  screenshot: string;
 }
 
-const data: readonly Project[] = [{ id: 1 }, { id: 2 }];
+const data: readonly Project[] = [
+  {
+    id: 1,
+    name: "Portfolio site (This site)",
+    githubFrontRepo: "https://github.com/a-sha0234/Portfolio-website",
+    githubBackRepo: "",
+    screenshot: "",
+  },
+  {
+    id: 2,
+    name: "Noories Blogs",
+    githubFrontRepo: "https://github.com/a-sha0234/blog-app-clientSide",
+    githubBackRepo: "https://github.com/a-sha0234/blog-app",
+    screenshot: blog,
+  },
+  {
+    id: 3,
+    name: "Receipt tracker",
+    githubFrontRepo: "https://github.com/a-sha0234/reciept-frontend",
+    githubBackRepo: "https://github.com/a-sha0234/receipt-backend",
+    screenshot: receipt,
+  },
+  {
+    id: 4,
+    name: "Pizza4u",
+    githubFrontRepo: "https://github.com/a-sha0234/Restaurent-landing-page",
+    githubBackRepo: "",
+    screenshot: restaurentImg,
+  },
+  {
+    id: 5,
+    name: "E-commerce app",
+    githubFrontRepo: "https://github.com/a-sha0234/Ecommerc-frontEnd",
+    githubBackRepo: "https://github.com/a-sha0234/Ecommerce-backend",
+    screenshot: "",
+  },
+
+  {
+    id: 6,
+    name: "Tic Tac Toe",
+    githubFrontRepo: "https://github.com/a-sha0234/tic-tac-toe-game",
+    githubBackRepo: "",
+    screenshot: ticTacToe,
+  },
+];
 
 export { data };
