f43bad832f3e313bbb9fc3a3da5994f2d8015103
a-sha0234 <asharma_31@hotmail.com>
2022-07-07 15:33:14 +0100

    add routes for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,9 +14,9 @@ function App() {
           <Navbar />
         </div>
         <Routes>
-          <Route />
-          <Route />
-          <Route />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
