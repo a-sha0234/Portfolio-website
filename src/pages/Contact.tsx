33089097f37bdff59e9cd035be2f87d2164be3b6
a-sha0234 <asharma_31@hotmail.com>
2022-07-08 13:24:24 +0100

    create contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,9 @@
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Contact Details</h1>
+      <p>Mobile: 07380434340</p>
+      <p>asharma10@hotmail.co.uk</p>
+    </div>
+  );
 }
