60741a3d8b45c5d938cce1819d9959b18d6dd8d4
a-sha0234 <asharma_31@hotmail.com>
2022-07-08 13:24:38 +0100

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,6 @@ export default function Home() {
         <h1>Arun Sharma</h1>
         <p>Computer science grad</p>
       </section>
-
-      <section>
-        <h1>Skills</h1>
-        <ul>
-          <li>HTML5</li>
-          <li>CSS</li>
-        </ul>
-      </section>
     </div>
   );
 }
