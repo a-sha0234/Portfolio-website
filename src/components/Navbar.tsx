148c2a8ab187efe311ae728f861ae92ef72dea1f
a-sha0234 <asharma_31@hotmail.com>
2022-07-09 22:11:55 +0100

    create responsive navbar and style it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,45 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+  const [isToggle, setIsToggle] = useState(false);
+
+  function handleToggle() {
+    setIsToggle(false);
+  }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/contact">Contact</Link>
-        </li>
+    <nav className="navbar">
+      {/* <div className="navbar__container"> */}
+      <h3 className="navbar__logo">Logo</h3>
+
+      <ul
+        className={isToggle ? "nav-links-mobile" : "navbar__nav-links"}
+        onClick={handleToggle}
+      >
+        <Link to="/" className="navbar__link">
+          <li> Home</li>
+        </Link>{" "}
+        <Link to="/about" className="navbar__link">
+          <li> About</li>
+        </Link>{" "}
+        <Link to="/projects" className="navbar__link">
+          <li> Projects</li>
+        </Link>{" "}
+        <Link to="/contact" className="navbar__link">
+          <li> Contact</li>
+        </Link>
       </ul>
+
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsToggle(!isToggle)}
+      >
+        {isToggle ? <FaTimes /> : <FaBars />}
+      </button>
+      {/* </div> */}
     </nav>
   );
 }
