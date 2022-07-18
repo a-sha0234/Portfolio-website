80fcc7eeff7d6729d1ce0cbac3fb7eda69d02806
a-sha0234 <asharma_31@hotmail.com>
2022-07-18 16:56:24 +0100

    add all changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,15 +4,7 @@ export default function About() {
       <h1 className="about__heading">About me</h1>
       <p className="about__text">
         I am a recent graduate of computer science looking for an opportunity to
-        apply my skills in a business setting. During my studies I got to work
-        with teams which allowed me to get a feel of a real workplace
-        environment, this helped me cultivate a handful of skills. To
-        illustrate, maintaining communication with other team members
-        religiously as the challenges we faced were diverse and always changing.
-        This required us to know when to ask for help and be organised enough in
-        order to be able to do so. This was instrumental into our success. I
-        look forward to working in a dynamic environment and translating skills
-        that I have developed to produce real results.
+        apply my skills in a software development setting. 
       </p>
       <h1 className="about__heading">Skill Set</h1>
 
